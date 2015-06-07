5b38779253956489a91b218c5c966700589ec605
Bashar Harfoush <bashar.harfoush@gmail.com>
2015-06-07 17:21:37 -0400

    Install required vim plugins

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,7 +8,15 @@ sudo -H -u vagrant bash << EOF
 git clone https://github.com/basharh/utilities
 ln -s utilities/home/.inputrc
 ln -s utilities/home/.vimrc
+ln -s utilities/home/.vim
 ln -s utilities/home/.zshenv
 ln -s utilities/home/.zshrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/jlanzarotta/bufexplorer
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/wesgibbs/vim-irblack
 EOF
 
