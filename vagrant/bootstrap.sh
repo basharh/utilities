e6bd0e4a94505b6d8682b4b8236377e81d356772
Bashar Harfoush <bashar.harfoush@gmail.com>
2015-06-07 16:56:16 -0400

    Fix home dir file permissions

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,10 +4,11 @@ apt-get -y install zsh
 apt-get -y install git
 apt-get -y install vim
 chsh -s /bin/zsh vagrant
-su vagrant; pwd;
+sudo -H -u vagrant bash << EOF
 git clone https://github.com/basharh/utilities
 ln -s utilities/home/.inputrc
 ln -s utilities/home/.vimrc
 ln -s utilities/home/.zshenv
 ln -s utilities/home/.zshrc
+EOF
 
